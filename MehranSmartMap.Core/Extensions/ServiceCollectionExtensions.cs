d41c2d509070ab83ee9f58da9017010af2806dba
rahmat <rahmat@rahmat-i>
2025-04-08 13:05:10 +0330

    add AddMehranSmartMap

---
+++ MehranSmartMap.Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MehranSmartMap.Core.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    // متد اکستنشن برای اضافه کردن تنظیمات SmartMap به سرویس‌ها
+    public static IServiceCollection AddMehranSmartMap(this IServiceCollection services)
+    {
+        // اضافه کردن AutoMapper به سرویس‌ها
+        services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
+
+        return services;
+    }
+}
